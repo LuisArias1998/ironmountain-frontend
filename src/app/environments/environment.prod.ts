878e9963ce0030c808b6e3901dee9ceee8702820
Luis Arias <ariasmontoya_luis@hotmail.com>
2023-05-02 19:00:26 -0700

    secure certificate

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    endpoint: "http://www.ironmountainapi.somee.com/api/Registro/"
+    endpoint: "https://www.ironmountainapi.somee.com/api/Registro/"
 }
